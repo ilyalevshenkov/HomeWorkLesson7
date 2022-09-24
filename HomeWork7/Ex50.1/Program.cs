9175d884be8c76b43b37a2de46a13526da0934ad
Ilya Levshenkov <111687010+ilyalevshenkov@users.noreply.github.com>
2022-09-24 14:16:13 +0300

    Add files via upload

---
+++ HomeWork7/Ex50.1/Program.cs
@@ -0,0 +1,36 @@
+﻿//  Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// Задаем массив m x n
+Console.WriteLine("Введите количество строк и столбцов массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array [i, j] = Convert.ToInt32(new Random().Next(0,21));  
+}
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i,j] + "\t  ");
+        Console.WriteLine();
+}
+//  Выводим нужный элемент массива на консоль
+ Console.WriteLine("Введите координаты элемента массива");
+ int a = Convert.ToInt32(Console.ReadLine());
+ int b = Convert.ToInt32(Console.ReadLine());
+ if (a>m && b>n)
+ Console.WriteLine("такого элемента нет");
+ else
+ {
+ object c = array.GetValue(a-1,b-1);
+ Console.WriteLine(c);
+ }
---
+++ HomeWork7/Ex52/Program.cs
@@ -0,0 +1,133 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3
+
+// Создание массива
+int LineTwoDimensionalArray()
+{
+    Console.Write("Введите колличество строк массива: ");
+    bool parseNIsOk = int.TryParse(Console.ReadLine(), out int line);
+    if (!parseNIsOk)
+    {
+        Console.WriteLine("Введено значение некорректного формата, введите заново");
+        return LineTwoDimensionalArray();
+    }
+    else
+    {
+        return line;
+    }
+}
+int ColumnTwoDimensionalArray()
+{
+    Console.Write("Введите колличество столбцов массива: ");
+    bool parseNIsOk = int.TryParse(Console.ReadLine(), out int column);
+    if (!parseNIsOk)
+    {
+        Console.WriteLine("Введено значение некорректного формата, введите заново");
+        return ColumnTwoDimensionalArray();
+    }
+    else
+    {
+        return column;
+    }
+}
+
+int[,] MakeTwoDimensionalArray()
+{
+    int line = LineTwoDimensionalArray();
+    int column = ColumnTwoDimensionalArray();
+    int[,] array = new int[line, column];
+    return array;
+}
+
+// Ввода начального и конечного чисел массива.
+int RandomNumbersStart()
+{
+    Console.Write("Введите первое число массива: ");
+    bool parseNIsOk = int.TryParse(Console.ReadLine(), out int start);
+    if (!parseNIsOk)
+    {
+        Console.WriteLine("Введено некорректное значение. Введите число заново");
+        return RandomNumbersStart();
+    }
+    else
+    {
+        return start;
+    }
+}
+int RandomNumbersEnd()
+{
+    Console.Write("Введите последнее число: ");
+    bool parseMIsOk = int.TryParse(Console.ReadLine(), out int finish);
+    if (!parseMIsOk)
+    {
+        Console.WriteLine("Введено некорректное значение. Введите число заново");
+        return RandomNumbersEnd();
+    }
+    else
+    {
+        return finish;
+    }
+}
+int[,] FillTwoDimensionalArray(int[,] array, int startRandom, int finishRandom)
+{
+    Random random = new Random();
+    int startValue = startRandom;
+    int endValue = finishRandom;
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+
+        {
+            array[i, j] = random.Next(startValue, endValue);
+        }
+    }
+    return array;
+}
+void WriteTwoDimensionalArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+//Среднее арифметическое каждого столбца:
+string ArithmeticMeanOfElements(int[,] array)
+{
+    string result = string.Empty;
+    double count = 0;
+
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        for (int j = 0; j < array.GetLength(0); j++)
+
+        {
+            count = count + array[j, i];
+        }
+
+        count = count / array.GetLength(0);
+        if (i == (array.GetLength(1) - 1)) result = result + count + ".";
+        else result = result + count + "; ";
+        count = 0;
+    }
+    return result;
+}
+
+int[,] array = MakeTwoDimensionalArray();
+int startRandom = RandomNumbersStart();
+int finishtRandom = RandomNumbersEnd();
+array = FillTwoDimensionalArray(array, startRandom, finishtRandom);
+WriteTwoDimensionalArray(array);
+Console.WriteLine();
+string result = ArithmeticMeanOfElements(array);
+Console.WriteLine($"Среднее арифметическое каждого столбца : {result}");
